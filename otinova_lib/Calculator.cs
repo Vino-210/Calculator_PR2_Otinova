2b6e53779658496c9e2db05a01f6cc9eb22e4673
Vino-210 <dasaotinova142@gmail.com>
2026-04-23 20:48:52 +0500

    Добавьте файлы проекта.

---
+++ otinova_lib/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace otinova_lib
+{
+    public static class Calculator
+    {
+        public const string DivideByZeroMessage = "Деление на ноль невозможно";
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException(DivideByZeroMessage);
+            }
+            return a / b;
+        }
+
+        public static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
---
+++ otinova_lib_tests/CalculatorTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using otinova_lib;
+
+namespace otinova_lib_tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            double a = 5;
+            double b = 3;
+            double expected = 8;
+
+            double actual = Calculator.Add(a, b);
+
+            Assert.AreEqual(expected, actual, 0.001, "Сложение работает неправильно");
+        }
+
+        [TestMethod]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            double a = 10;
+            double b = 4;
+            double expected = 6;
+
+            double actual = Calculator.Subtract(a, b);
+
+            Assert.AreEqual(expected, actual, 0.001, "Вычитание работает неправильно");
+        }
+
+        [TestMethod]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            double a = 6;
+            double b = 7;
+            double expected = 42;
+
+            double actual = Calculator.Multiply(a, b);
+
+            Assert.AreEqual(expected, actual, 0.001, "Умножение работает неправильно");
+        }
+
+        [TestMethod]
+        public void Divide_TwoNumbers_ReturnsQuotient()
+        {
+            double a = 15;
+            double b = 3;
+            double expected = 5;
+
+            double actual = Calculator.Divide(a, b);
+
+            Assert.AreEqual(expected, actual, 0.001, "Деление работает неправильно");
+        }
+
+        [TestMethod]
+        public void Divide_ByZero_ThrowsDivideByZeroException()
+        {
+            double a = 10;
+            double b = 0;
+
+            Assert.ThrowsException<DivideByZeroException>(() => Calculator.Divide(a, b));
+        }
+
+        [TestMethod]
+        public void Divide_ByZero_ThrowsDivideByZeroException_WithCorrectMessage()
+        {
+            double a = 10;
+            double b = 0;
+
+            try
+            {
+                Calculator.Divide(a, b);
+                Assert.Fail("Исключение не было выброшено");
+            }
+            catch (DivideByZeroException ex)
+            {
+                StringAssert.Contains(ex.Message, Calculator.DivideByZeroMessage);
+            }
+        }
+
+        [TestMethod]
+        public void Power_NumberAndExponent_ReturnsPoweredValue()
+        {
+            double a = 2;
+            double b = 3;
+            double expected = 8;
+
+            double actual = Calculator.Power(a, b);
+
+            Assert.AreEqual(expected, actual, 0.001, "Возведение в степень работает неправильно");
+        }
+    }
+}
